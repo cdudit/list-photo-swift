c943925b9255f20b5dbaf2a44a7dda15ff398f4c
Clément Dudit <clement.dudit85@gmail.com>
2022-04-06 09:28:30 +0200

    Initial Commit

---
+++ ListPhotoSwift/ListPhotoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListPhotoSwiftApp.swift
+//  ListPhotoSwift
+//
+//  Created by Clément Dudit on 06/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ListPhotoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
